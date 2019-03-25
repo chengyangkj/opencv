90b9a370f332ebc334b5ecb2575247f668feb668
chengyangkj <1419478526@qq.com>
2019-03-25 23:04:38 +0800

    人流量识别

--- opencv-人流量识别/opencv-人流量识别/main.cpp
+++ opencv-人流量识别/opencv-人流量识别/main.cpp
@@ -1,13 +1,14 @@
 #include <opencv2\opencv.hpp>
 #include <iostream>
-#include <string.h>
+#include <string.h> 
 using namespace cv;
 using namespace std;
-
+void get_change(Mat fram);
 //定义训练结果的目录
-String fileName = "F:/opencv/opencv/sources/data/haarcascades/haarcascade_lowerbody.xml";
+String fileName = "F:/opencv/InputImg/people/cascade.xml";
 //定义模型对象
 CascadeClassifier face_classifier;
+int flag = 0;
 int main() {
 	//加载模型
 	if (!face_classifier.load(fileName)) {
@@ -15,26 +16,43 @@ int main() {
 		return -1;
 	}
 	VideoCapture capture;
-	capture.open("F:/opencv/InputImg/外走廊_人多.mp4");
+	capture.open("F:/opencv/InputImg/people/123.mp4");
 	Mat fram;
 	Mat gray;
+	namedWindow("detect faces", CV_WINDOW_AUTOSIZE);
+	//定义矩形容器 将检测的结果放入 
+	vector<Rect> faces;
 	while (capture.read(fram))
 	{
 		//转换为灰度图像
 		cvtColor(fram, gray, COLOR_BGR2GRAY);
 		//直方图均衡化 
 		equalizeHist(gray, gray);
-		//定义矩形容器 将检测的结果放入 
-		vector<Rect> faces;
+	
 		//检测图像 Size表示最小接受的对象的大小
-		face_classifier.detectMultiScale(gray, faces, 1.2, 3, 0, Size(24, 24));
-		//在结果上画矩形
+		face_classifier.detectMultiScale(gray, faces, 1.3, 8, 0, Size(18, 18));
+		//画线
+		Point p1 = Point(0, fram.rows / 2);   //坐标
+		Point p2 = Point(fram.cols, fram.rows / 2);
+		Scalar color = Scalar(255, 0, 255);
+		line(fram, p1, p2, color, 1, LINE_8);  //画线
+		Point roi;
+		int roi_row;
+		int roi_col;
 		for (size_t t = 0; t < faces.size(); t++) {
-			rectangle(gray, faces[static_cast<int>(t)], Scalar(0, 0, 255), 2, 8, 0);
+			rectangle(fram, faces[t], Scalar(0, 0, 255), 2, 8, 0);
+			//将目标范围内某个像素改为绿色
+			roi.x = faces[t].x*1.1;
+			roi.y = faces[t].y*1.1;
+			//cout << fram.at<Vec3b>(roi_row, roi_col)[0] << endl;
+			//fram.at<Vec3b>(roi_row, roi_col)[1] = 255;
+			//fram.at<Vec3b>(roi_row, roi_col)[2] = 0;
+			circle(fram, roi, 3, Scalar(0, 255, 0), -3); //第五个参数我设为-1，表明这是个实点。		
 		}
-		namedWindow("detect faces", CV_WINDOW_AUTOSIZE);
+		get_change(fram);
 		imshow("detect faces", fram);
-		char c = waitKey(50);
+		cout << flag << endl;
+		char c = waitKey(30);
 		//当输入的按键为ESC时退出
 		if (c == 27) {
 			break;
@@ -43,4 +61,15 @@ int main() {
 
 	waitKey(0);
 	return 0;
+}
+void get_change(Mat fram) {
+	int height = fram.rows;
+	int width = fram.cols;
+	int row = height / 2;
+	//cout << fram.at<Vec3b>(row,3) << endl;
+	for (int col = 0; col <width; col++) {
+		if (fram.at<Vec3b>(row, col)[1] == 255) {
+			flag++;
+		}
+	}
 }
--- test/源.cpp
+++ test/源.cpp
@@ -1,35 +1,58 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include<opencv2/opencv.hpp>
+#include<iostream>
 
 using namespace cv;
 using namespace std;
 
-String fileName = "F:/opencv/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";
-CascadeClassifier face_classifier;
+CascadeClassifier face_cascader;
+CascadeClassifier eye_cascader;
+String facefile = "F:/opencv/InputImg/people/cascade.xml";
+String eyefile = "F:/opencv/opencv/sources/data/haarcascades/haarcascade_eye.xml";
 
 int main(int argc, char** argv) {
-	if (!face_classifier.load(fileName)) {
+	if (!face_cascader.load(facefile)) {
 		printf("could not load face feature data...\n");
 		return -1;
 	}
-
-	Mat src = imread("F:/opencv/InputImg/man1.png");
-	if (src.empty()) {
-		printf("could not load image...\n");
+	if (!eye_cascader.load(eyefile)) {
+		printf("could not load eye feature data...\n");
 		return -1;
 	}
-	imshow("input image", src);
+	namedWindow("camera-demo", CV_WINDOW_AUTOSIZE);
+	VideoCapture capture;
+	capture.open("F:/opencv/InputImg/people/123.mp4");
+	Mat frame;
 	Mat gray;
-	cvtColor(src, gray, COLOR_BGR2GRAY);
-	equalizeHist(gray, gray);
-
 	vector<Rect> faces;
-	face_classifier.detectMultiScale(gray, faces, 1.2, 3, 0, Size(24, 24));
-	for (size_t t = 0; t < faces.size(); t++) {
-		rectangle(src, faces[static_cast<int>(t)], Scalar(0, 0, 255), 2, 8, 0);
+	vector<Rect> eyes;
+	while (capture.read(frame)) {
+		cvtColor(frame, gray, COLOR_BGR2GRAY);
+		equalizeHist(gray, gray);
+		face_cascader.detectMultiScale(gray, faces, 1.2, 6, 0, Size(50, 50));
+		for (size_t t = 0; t < faces.size(); t++) {
+			Rect roi;
+			roi.x = faces[static_cast<int>(t)].x;
+			roi.y = faces[static_cast<int>(t)].y;
+			roi.width = faces[static_cast<int>(t)].width;
+			roi.height = faces[static_cast<int>(t)].height / 2;
+			Mat faceROI = frame(roi);
+			eye_cascader.detectMultiScale(faceROI, eyes, 1.2, 3, 0, Size(20, 20));
+			for (size_t k = 0; k < eyes.size(); k++) {
+				Rect rect;
+				rect.x = faces[static_cast<int>(t)].x + eyes[k].x;
+				rect.y = faces[static_cast<int>(t)].y + eyes[k].y;
+				rect.width = eyes[k].width;
+				rect.height = eyes[k].height;
+				rectangle(frame, rect, Scalar(0, 255, 0), 2, 8, 0);
+			}
+			rectangle(frame, faces[static_cast<int>(t)], Scalar(0, 0, 255), 2, 8, 0);
+		}
+		imshow("camera-demo", frame);
+		char c = waitKey(30);
+		if (c == 27) {
+			break;
+		}
 	}
-
-	imshow("detect faces", src);
 	waitKey(0);
 	return 0;
 }
